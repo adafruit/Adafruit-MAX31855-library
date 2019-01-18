222bf444c82418414f528e1b4dba37b96b915e1d
Dominic Clifton <me@dominicclifton.name>
2019-01-18 13:27:25 +0100

    Fix too many `F`'s in negative value calculation.

--- Adafruit_MAX31855.cpp
+++ Adafruit_MAX31855.cpp
@@ -100,7 +100,7 @@ double Adafruit_MAX31855::readCelsius(void) {
 
   if (v & 0x80000000) {
     // Negative value, drop the lower 18 bits and explicitly extend sign bits.
-    v = 0xFFFFC000 | ((v >> 18) & 0x00003FFFF);
+    v = 0xFFFFC000 | ((v >> 18) & 0x00003FFF);
   }
   else {
     // Positive value, just drop the lower 18 bits.
