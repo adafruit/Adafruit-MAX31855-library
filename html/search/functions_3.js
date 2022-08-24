751e1f6e3b51c51d7c9f9ba621907cde25cae8ae
Doxygen CI <ci-arduino@invalid>
2022-08-24 18:13:54 +0000

    Deploy docs to GitHub Pages from commit 2a860b0de1
    Commit: 2a860b0de10d9bf2e5ca5dd7b03d1e6c8b88550e
    GitHub Actions run: 2921289826

---
+++ html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['setfaultchecks',['setFaultChecks',['../class_adafruit___m_a_x31855.html#aadcaf7feec433c48219c9d3764fd67ce',1,'Adafruit_MAX31855']]]
+];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,10 +1,11 @@
 var indexSectionsWithContent =
 {
-  0: "abr",
+  0: "abmrs",
   1: "a",
   2: "a",
-  3: "abr",
-  4: "a"
+  3: "abrs",
+  4: "m",
+  5: "a"
 };
 
 var indexSectionNames =
@@ -13,7 +14,8 @@ var indexSectionNames =
   1: "classes",
   2: "files",
   3: "functions",
-  4: "pages"
+  4: "defines",
+  5: "pages"
 };
 
 var indexSectionLabels =
@@ -22,6 +24,7 @@ var indexSectionLabels =
   1: "Classes",
   2: "Files",
   3: "Functions",
-  4: "Pages"
+  4: "Macros",
+  5: "Pages"
 };
 
